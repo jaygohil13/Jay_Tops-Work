e85ff49d0133eddd2765aa1b1c7241ae9e421238
jaygohil13 <gohiljayb10@gmail.com>
2022-12-07 10:41:17 +0530

    upload

---
+++ Python_Language/Class_Works/List_slicing.py
@@ -0,0 +1,15 @@
+# List slicing :
+
+# starting index - 0
+
+l1 = [12,34,67,8,34,6,2,8]
+
+print(l1[:3]) # first 3 values
+  
+print(l1[2:5]) # between values
+
+print(l1[-1]) # last value
+
+print(l1[-4:-1])
+
+print(l1[-4:])
