a46502d211df23030bf0b093e37111cd1071650d
jaygohil13 <gohiljayb10@gmail.com>
2022-12-14 10:20:31 +0530

    upload

--- Python_Language/Daily_Tasks/Function_Task.py
+++ Python_Language/Daily_Tasks/Function_Task.py
@@ -1,40 +1,47 @@
-def switch():
-    n1 = int(input("Enter number 1 : "))
-    n2 = int(input("Enter number 2 : "))
+def menu():
+    menu = """
+           press 1 for addition
+           press 2 for multiplication
+           press 3 for subtraction
+           press 4 for division
+    
+    """
+    print(menu)
 
-    print("press 1 for addition \npress 2 for subtraction \npress 3 for multiplication \npress 4 for division")
+def Addition(num1,num2):
+    ans = num1 + num2
+    print(ans)
 
-choice = " "
-if choice == 1:
-    ans = (n1 + n2)
-    print("Addition : ", ans)
-elif choice == 2:
-    ans = (n1 + n2)
-    print("subtraction : ",ans)
-elif choice == 3:
-    ans = (n1 * n2)
-    print("multiplication : ",ans)
-elif choice == 4:
-    ans = (n1 / n2)
-    print("division : ",ans)
-else:
-    print("invalid choice")
+def  Multiplication(num1,num2):
+    ans = num1 * num2
+    print(ans)
 
+def Subtraction(num1,num2):
+    ans = num1 - num2
+    print(ans)
 
+def Division(num1,num2):
+    ans = num1 / num2 
+    print(ans)
 
+status = True
+while status:
+    menu()
+    num1 = int(input("Enter number 1 : "))
+    num2 = int(input("Enter number 2 : "))
 
+    choice = int(input("Enter your choice : "))
+    if choice == 1:
+        Addition(num1,num2)
+    elif choice == 2:
+        Multiplication(num1,num2)
+    elif choice == 3:
+        Subtraction(num1,num2)
+    elif choice == 4:
+        Division(num1,num2)
+    else:
+        print("Invalid choice : ")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+    s = input("Do you want to enter more operations : ")
+    if s == 'n':
+       status = False
