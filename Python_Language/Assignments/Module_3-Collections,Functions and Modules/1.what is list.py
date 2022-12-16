1f944b42663d9dd212c7b6ecd61900cca6cdf723
jaygohil13 <gohiljayb10@gmail.com>
2022-12-16 15:21:44 +0530

    upload

---
+++ Python_Language/Assignments/Module_3-Collections,Functions and Modules/1.what is list.py
@@ -0,0 +1,17 @@
+"""
+1) What is List ? How will you reverse a list ?
+
+-->> List :list is a collection data-type which is contain similar and dis-similar data elements at single location.
+
+-->> list is represent by square brackets [].
+
+2) Reverse a List : a built-in function called reverse() is used to reverse a list.
+
+syntax :
+
+"""
+l1 = [25,32,45,65,24,]
+
+l1.reverse()
+
+print(l1)
