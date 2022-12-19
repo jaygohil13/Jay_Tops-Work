35097fbcbe29c1bf5b0cb8802a6e8a83d57666a4
jaygohil13 <gohiljayb10@gmail.com>
2022-12-19 10:35:24 +0530

    upload

--- Python_Language/Class_Works/Write_file_example.py
+++ Python_Language/Class_Works/Write_file_example.py
@@ -1,7 +1,19 @@
+"""
+
 f = open("C:\\Users\\gohil\\OneDrive\\Documents\\GitHub\\Jay_Tops-Work\\Python_Language\\Class_Works\\myfile_write.txt","w")
 
 
 name = input("Enter anything : ")
 f.write(name)
+
+f.close()
+
+"""
+
+f = open("C:\\Users\\gohil\\OneDrive\\Documents\\GitHub\\Jay_Tops-Work\\Python_Language\\Class_Works\\myfile_write.txt","a")
+
+name = input("Enter anything : ")
+f.write("\n"+name)
+
 
 f.close()
--- Python_Language/Daily_Tasks/Quiz_Game.py
+++ Python_Language/Daily_Tasks/Quiz_Game.py
@@ -1,43 +1,63 @@
-print("Welcome To Quiz Compitition")
-username = input("username : ")
+print("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=")
+print("\t\t\t\tWELCOME TO KBC COMPETITION")
+print("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=")
+username = input("User Name : ")
+
+right_ans = 0
+wrong_ans = 0
 
 points = 0
 playing = True
 while playing == True:
    
 
-    print(" 1.What is jersey number of virat kohli ?")
-    Answer_1 = input("Fill in here : ")
+    print("\nQuestion : 1.What is jersey number of virat kohli ?")
+    Answer_1 = input("\n--> Fill The Answer Here : ")
     if Answer_1 == "18" or Answer_1 == "18":
        points += 10
-       print("Correct !")
+       print("points : ",points)
+       right_ans += 1
+       print("Correct Answer!")
        
     else:
         points -= 20
-        print("Wrong !")
+        print("points : ",points)
+        wrong_ans += 1
+        print("Wrong Answer!")
         
 
-    print("2.What is the capital of india ?")
-    Answer_2 = input("Fill in here : ")
+    print("\nQuestion : 2.What is the capital of india ?")
+    Answer_2 = input("\n-->Fill The Answer Here : ")
     if Answer_2 == "New Delhi" or Answer_2 == "new delhi":
-        print("Correct !")
         points +=10
-    else:
-        print("Wrong !")
-        points -= 20
-
-    print("3.Who is india's prime minister ?")
-    Answer_3 = input("Fill in here : ")
-    if Answer_3 == "Narendra Modi" or Answer_3 =="narendra modi":
+        print("points : ",points)
+        right_ans += 1
         print("Correct !")
-        points += 10
-    else:
-        print("Wrong !")
-        points -= 20
 
-    print("points :",points)
-    
-    choice = input("do you want to play again ? : ")
+    else:
+        points -= 20
+        print("points : ",points)
+        wrong_ans += 1
+        print("Wrong !")
+
+    print("\nQuestion : 3. Who is india's prime minister ?")
+    Answer_3 = input("\n-->Fill The Answer Here : ")
+    if Answer_3 == "Narendra Modi" or Answer_3 =="narendra modi":
+        points += 10
+        print("points : ",points)
+        right_ans += 1
+        print("Correct !")
+    else:
+        points -= 20
+        print("points : ",points)
+        wrong_ans += 1
+        print("Wrong !")
+
+    print("\nTotal points :",points)
+    print("\nTotal right answers are : ",right_ans)
+    print("Total wrong answers are : ",wrong_ans)
+
+    choice = input("\ndo you want to play again ? : ")
     if choice == 'n' or choice == "no":
 
         playing = False
